edbe4af7b840edaad68d3a1728c19d2d1c586089
lili_ana <lilianah33446@gmail.com>
2024-11-25 01:44:12 -0500

    god pls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { EmailProvider } from './Context/EmailContext';
 import { VerifCodeProvider } from './Context/VerificationCodeContext';
 import { ButtonContext, ButtonProvider } from './Context/ButtonContext';
